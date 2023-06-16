634ce2c0ab975c4f73be7ef3b7cdf13416561075
arionrefat <arionrefat@gmail.com>
2023-06-16 11:51:47 +0600

    fixed team page

--- gamezeusWeb/app/team/page.tsx
+++ gamezeusWeb/app/team/page.tsx
@@ -17,34 +17,33 @@ export const metadata = {
 
 export default async function TeamPage() {
   return (
-    <div className="container max-w-4xl py-6 sm:max-w-min lg:py-10">
+    <div className="container p-6">
       <h1 className="pb-4 text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
         Meet the Team
       </h1>
       <Separator className="pb-1" />
-      <div className="flex flex-col items-start gap-4 pt-4 md:flex-row md:justify-between md:gap-8">
-        <div className="flex flex-col space-y-4 xl:flex-row xl:space-x-4 xl:space-y-0">
-          {TeamDescription.map((author, index) => (
-            <Card className="w-full md:w-96" key={index}>
-              <CardHeader>
-                <Image
-                  src={author.imgSrc}
-                  alt={author.altName}
-                  height={100}
-                  width={384}
-                />
-                <CardTitle className="pt-2">{author.altName}</CardTitle>
-              </CardHeader>
-              <CardContent>{author.designation}</CardContent>
-              <CardFooter>
-                <CardDescription className="text-left">
-                  {author.bio}
-                </CardDescription>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
+      <div className="grid grid-cols-1 gap-4 pt-4 md:grid-cols-2 md:gap-8 xl:grid-cols-3">
+        {TeamDescription.map((author, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <Image
+                src={author.imgSrc}
+                alt={author.altName}
+                height={100}
+                width={384}
+              />
+              <CardTitle className="pt-2">{author.altName}</CardTitle>
+            </CardHeader>
+            <CardContent>{author.designation}</CardContent>
+            <CardFooter>
+              <CardDescription className="text-left">
+                {author.bio}
+              </CardDescription>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
   )
 }
+
